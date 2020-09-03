9b85c6e9bb7febee17a29631588ae2809d5d06fe
Julliann Dulay <jullianndulay@gmail.com>
2020-09-03 15:08:50 -0700

    Finished and checked mvp

--- src/main/java/com/lambdaschool/countries/controllers/CountryController.java
+++ src/main/java/com/lambdaschool/countries/controllers/CountryController.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 @RestController
@@ -33,18 +35,38 @@ public class CountryController {
     //invokes method when the route is the same
     @GetMapping(value = "/names/all", produces = {"application/json"})
     public ResponseEntity<?> listAllCountries(){
-        //create list
         List<Country> myList = new ArrayList<>();
-        //found all employees and added to created list
         countryrepos.findAll().iterator().forEachRemaining(myList::add);
-        //sort list by last name
-        myList.sort((e1,e2) -> e1.getName().compareToIgnoreCase(e2.getName()));
+        myList.sort((c1,c2) -> c1.getName().compareToIgnoreCase(c2.getName()));
         //print to console
-        for(Country e : myList){
-            System.out.println(e);
+        for(Country c : myList){
+            System.out.println(c);
         }
         //return to client
         return new ResponseEntity<>(myList, HttpStatus.OK);
     }
 
+    //http://localhost:2019/names/start/u
+    @GetMapping( value = "/names/start/{letter}", produces = {"application/json"})
+    public ResponseEntity<?> listAllByFirstLetter(@PathVariable char letter) {
+        List<Country> myList = new ArrayList<>();
+        countryrepos.findAll().iterator().forEachRemaining(myList::add);
+        List<Country> newList = findCountries(myList, c->c.getName().toLowerCase().charAt(0) == letter);
+        return new ResponseEntity<>(newList,HttpStatus.OK);
+    }
+
+    @GetMapping( value = "/population/min", produces = {"application/json"})
+    public ResponseEntity<?> getMinPopulation() {
+        List<Country> myList = new ArrayList<>();
+        countryrepos.findAll().iterator().forEachRemaining(myList::add);
+        myList.sort((c1, c2) -> (int)(c1.getPopulation() - c2.getPopulation()));
+        return new ResponseEntity<>(myList.get(0),HttpStatus.OK);
+    }
+    @GetMapping( value = "/population/max", produces = {"application/json"})
+    public ResponseEntity<?> getMaxPopulation() {
+        List<Country> myList = new ArrayList<>();
+        countryrepos.findAll().iterator().forEachRemaining(myList::add);
+        myList.sort((c1, c2) -> (int)(c1.getPopulation() - c2.getPopulation()));
+        return new ResponseEntity<>(myList.get(myList.size()-1),HttpStatus.OK);
+    }
 }
