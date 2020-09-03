6880c50d2409ace9675802e5ec1f83952d68dd63
Julliann Dulay <jullianndulay@gmail.com>
2020-09-03 14:27:57 -0700

    Initial commit done

---
+++ src/main/java/com/lambdaschool/countries/repositories/CountryRepository.java
@@ -0,0 +1,6 @@
+package com.lambdaschool.countries.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CountryRepository extends CrudRepository<Country, Long> {
+}
